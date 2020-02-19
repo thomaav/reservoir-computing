Fix waxman by adding negative signs as well.

py/ESN.py
@@ -61,9 +61,11 @@ class ESN(nn.Module):
 
         if self.w_res_type == 'waxman':
             G = waxman(n=self.hidden_nodes, alpha=1.0, beta=1.0,
-                       connectivity='global', z_frac=1.0, scale=0.01)
+                       connectivity='global', z_frac=1.0, scale=1.0,
+                       directed=True)
             A = nx.to_numpy_matrix(G)
             w_res = torch.FloatTensor(A)
+            w_res *= self.spectral_radius / _spectral_radius(w_res)
 
         if self.w_in_distrib == Distribution.gaussian:
             w_in = torch.empty(self.hidden_nodes).normal_(mean=0.0, std=1.0)
@@ -82,7 +84,7 @@ class ESN(nn.Module):
         self.register_buffer('w_out', w_out)
 
 
-    def forward(self, u, y=None, u_mc=None):
+    def forward(self, u, y=None, u_mc=None, plot=False):
         timeseries_len = u.size()[0]
         X = torch.zeros(timeseries_len, self.output_dim)
         x = torch.zeros(self.hidden_nodes)
@@ -111,6 +113,11 @@ class ESN(nn.Module):
         # Record the previous time series passed through the reservoir.
         self.X = X
         self.v = v
+
+        if plot:
+            import matplotlib.pyplot as plt
+            plt.plot(self.X)
+            plt.show()
 
         X = X[self.washout:]
         y = y[self.washout:] if y is not None else y

py/matrix.py
@@ -12,13 +12,14 @@ def euclidean(x, y):
 
 
 
-def waxman(n, alpha, beta, connectivity='default', z_frac=1.0, scale=1.0):
+def waxman(n, alpha, beta, connectivity='default', z_frac=1.0, scale=1.0,
+           directed=False):
     """
     B. M. Waxman, Routing of multipoint connections.
 
     Adapted from the NetworkX implementation.
     """
-    G = nx.Graph()
+    G = nx.DiGraph() if directed else nx.Graph()
     G.add_nodes_from(range(n))
 
     domain = (0, 0, 0, 1, 1, 1)
@@ -42,7 +43,10 @@ def waxman(n, alpha, beta, connectivity='default', z_frac=1.0, scale=1.0):
     elif connectivity == 'global':
         for pair in combinations(G, 2):
             u, v = pair[0], pair[1]
-            G.add_edge(u, v, weight=euclidean(pos[u], pos[v])*scale)
+            if directed:
+                u, v = (u, v) if np.random.random() < .5 else (v, u)
+            weight_sign = 1 if np.random.random() < .5 else -1
+            G.add_edge(u, v, weight=weight_sign*euclidean(pos[u], pos[v])*scale)
 
     return G
 