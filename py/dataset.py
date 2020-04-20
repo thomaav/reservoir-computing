494414cf37ffedf1744f26c89470e0cb6890d367
Thomas Aven <avensin@hotmail.com>
2020-04-20 16:35:19 +0200

    Big NARMA.

--- py/dataset.py
+++ py/dataset.py
@@ -24,22 +24,22 @@ def load_dataset():
 dataset = None
 
 def NARMA(sample_len, system_order=10):
-    if system_order != 10:
-        raise NotImplementedError('NARMA only supported for system order of 10')
-
     alpha = 0.3
     beta = 0.05
     gamma = 1.5
     delta = 0.1
+    n = system_order
 
     u = torch.rand(sample_len) * 0.5
     y = torch.zeros(sample_len)
 
-    for t in range(10, sample_len):
+    for t in range(n, sample_len):
         y[t] = alpha*y[t-1] + \
-               beta*y[t-1]*torch.sum(y[t-10:t]) + \
-               gamma*u[t-1]*u[t-10] + \
+               beta*y[t-1]*torch.sum(y[t-n:t]) + \
+               gamma*u[t-1]*u[t-n] + \
                delta
+        if n != 10:
+            y[t] = np.tanh(y[t])
 
     if not np.isfinite(y).all():
         class DivergentTimeseriesError(Exception):
